5e68554f5e4c201d0008a6881669fd682f5c2a90
E.Vlad <ejohn86@gmail.com>
2015-12-05 11:24:33 +0300

    Sceleton2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 var App = {};
 
-App.init = function(){
+App.init = function() {
+	App.loadTemplate('app', {
+		title: 'Привет'
+	}, "#main-view")
 }
 
-window.onload = function(){
+window.onload = function() {
 	App.init();
 }
+
+App.loadTemplate = function(view, data, target) {
+	var swig = require('swig'),
+		fileName = 'view/' + view + '.html';
+	var template = swig.renderFile(fileName, data || {});
+	if (target)
+		return $(target).html(template);
+	return template;
+};
